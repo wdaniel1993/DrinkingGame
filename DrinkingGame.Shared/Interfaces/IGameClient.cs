b0e4552682a29719e9858dd25fb3f3bbd242f765
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2017-04-07 17:09:49 +0200

    add interfaces

---
+++ DrinkingGame.Shared/Interfaces/IGameClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrinkingGame.Shared.Interfaces
+{
+    public interface IGameClient
+    {
+        void UpdateGameDetails(IEnumerable<string> players);
+        void NewQuestion(string question);
+        void NewAnswer(string question, string player);
+        void ShouldDrink(string player);
+        void UpdateScores(Dictionary<string,int> scores);
+    }
+}
---
+++ DrinkingGame.Shared/Interfaces/IGameServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrinkingGame.Shared.Interfaces
+{
+    public interface IGameServer
+    {
+        void ConnectToGame(int gameNumber, bool supportShouldDrink);
+        void PlayerDrank(string player);
+        void GaveAnswer(string player, int answer);
+    }
+}
