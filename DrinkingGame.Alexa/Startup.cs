f7b0e5ad53248b29e8120969827d12e77552f51d
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2017-04-09 15:56:53 +0200

    Startup with owin

---
+++ DrinkingGame.Alexa/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(DrinkingGame.Alexa.Startup))]
+
+namespace DrinkingGame.Alexa
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            var httpConfiguration = CreateHttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            app.UseWebApi(httpConfiguration);
+            app.MapSignalR();
+        }
+
+        public static HttpConfiguration CreateHttpConfiguration()
+        {
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+
+            return httpConfiguration;
+        }
+    }
+}
