72b22738c6e72cc83cdd750c680f51cd1892c633
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2017-04-21 14:11:41 +0200

    add signalr client

--- DrinkingGame.BusinessLogic/Machine/StateFactory.cs
+++ DrinkingGame.BusinessLogic/Machine/StateFactory.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DrinkingGame.BusinessLogic.States;
 
 namespace DrinkingGame.BusinessLogic.Machine
 {
     public class StateFactory : IStateFactory
     {
+        public IState GameStarting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState Initializing()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState RoundStarting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState AnswerReading()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState LoserDrinking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState RoundEnding()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState GameEnding()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DrinkingGame.Shared/Interfaces/IGameClient.cs
+++ DrinkingGame.Shared/Interfaces/IGameClient.cs
@@ -9,6 +9,7 @@ namespace DrinkingGame.Shared.Interfaces
         void UpdateGameDetails(IEnumerable<string> players);
         void NewQuestion(string question);
         void NewAnswer(string question, string player);
+        void CorrectAnswer(string question, string answer);
         void ShouldDrink(string player);
         void UpdateScores(Dictionary<string,int> scores);
     }
