bd03330db8c382e2d36c7250198f8c170e056ba0
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2017-05-22 22:16:57 +0200

    Rename Assembly

--- DrinkingGame.Alexa/Properties/AssemblyInfo.cs
+++ DrinkingGame.Alexa/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DrinkingGame.Alexa")]
+[assembly: AssemblyTitle("DrinkingGame.WebService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DrinkingGame.Alexa")]
+[assembly: AssemblyProduct("DrinkingGame.WebService")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
