5c180a87faeab14b6654d0fcf91a5bddbd152dfc
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2017-06-08 12:30:35 +0200

    Finish Game

--- DrinkingGame.Alexa/Dialogs/GameDialog.cs
+++ DrinkingGame.Alexa/Dialogs/GameDialog.cs
@@ -135,7 +135,7 @@ namespace DrinkingGame.WebService.Dialogs
                 if (await TryAction(game.CompleteCurrentRound(true), context))
                 {
                     await WriteScores(context, game);
-                    await Answer(context, "Finished Game");
+                    await Answer(context, "Finished Game", InputHints.AcceptingInput);
                 }
             }
         }
